6bb36885bf2beeec96be2d72d59653adb90bbded
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 23:30:36 +0900

    feat main to qna

--- JsProject/MBTI_test/start/js/start.js
+++ JsProject/MBTI_test/start/js/start.js
@@ -4,8 +4,14 @@ const qna = document.querySelector("#qna");
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    qna.style.WebkitAnimation = "fadeIn 1s";
+    qna.style.animation = "fadeIut 1s";
+    setTimeout(() => {
+      main.style.display = "none";
+      qna.style.display = "block";
+    }, 450);
+  }, 450);
   qna.style.WebkitAnimation = "fadeIn 1s";
   qna.style.animation = "fadeIut 1s";
-  // main.style.display = "none";
-  // qna.style.display = "block";
 }
