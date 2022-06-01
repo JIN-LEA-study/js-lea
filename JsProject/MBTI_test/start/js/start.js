d6b71bcb42a495356dafbada78afd6a69edf806b
leaisrevolution <leaisrevolution@gmail.com>
2022-06-01 23:42:43 +0900

    feat qna button

--- JsProject/MBTI_test/start/js/start.js
+++ JsProject/MBTI_test/start/js/start.js
@@ -1,6 +1,21 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText) {
+  let a = document.querySelector(".answerBox");
+  let answer = document.createElement("button");
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+}
+
+function goNext(qIex) {
+  let q = document.querySelector(".qBox");
+  q.innerHTML = qnaList[qIex].q;
+  for (let i in qnaList[qIex].a) {
+    addAnswer(qnaList[qIex].a[i].answer);
+  }
+}
+
 function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
@@ -11,7 +26,7 @@ function begin() {
       main.style.display = "none";
       qna.style.display = "block";
     }, 450);
+    let qIdex = 0;
+    goNext(qIdex);
   }, 450);
-  qna.style.WebkitAnimation = "fadeIn 1s";
-  qna.style.animation = "fadeIut 1s";
 }
