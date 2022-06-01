9af69e162292ac405e95aa6375c55df7f1db24e4
leaisrevolution <leaisrevolution@gmail.com>
2022-06-02 01:01:15 +0900

    [feat] calResult

--- JsProject/MBTI_test/start/js/start.js
+++ JsProject/MBTI_test/start/js/start.js
@@ -1,22 +1,66 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
+
 const endPoint = 12;
+const select = [];
+
+function calResult() {
+  let pointArray = [
+    { name: "mouse", value: 0, key: 0 },
+    { name: "cow", value: 0, key: 1 },
+    { name: "tiger", value: 0, key: 2 },
+    { name: "rabbit", value: 0, key: 3 },
+    { name: "dragon", value: 0, key: 4 },
+    { name: "snake", value: 0, key: 5 },
+    { name: "horse", value: 0, key: 6 },
+    { name: "sheep", value: 0, key: 7 },
+    { name: "monkey", value: 0, key: 8 },
+    { name: "chick", value: 0, key: 9 },
+    { name: "dog", value: 0, key: 10 },
+    { name: "pig", value: 0, key: 11 },
+  ];
+
+  for (let i = 0; i < endPoint; i++) {
+    let target = qnaList[i].a[select[i]];
+    for (let j = 0; j < target.lenghtl; j++) {
+      for (let k = 0; k < pointArray.length; k++) {
+        if (target.type[j] === pointArray[k].name) {
+          pointArray[k].value += 1;
+        }
+      }
+    }
+  }
+
+  let resultArray = pointArray.sort(function (a, b) {
+    if (a.value > b.value) {
+      return -1;
+    }
+    if (a.value < b.value) {
+      return 1;
+    }
+    return 0;
+  });
+
+  let resultword = resultArray[0].key;
+  return resultword;
+}
 
 function goResult() {
   qna.style.WebkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
   setTimeout(() => {
     result.style.WebkitAnimation = "fadeIn 1s";
-    result.style.animation = "fadeIut 1s";
+    result.style.animation = "fadeIn 1s";
     setTimeout(() => {
-      main.style.display = "none";
-      qna.style.display = "block";
+      qna.style.display = "none";
+      result.style.display = "block";
     }, 450);
   });
+  console.log(select);
 }
 
-function addAnswer(answerText, qIdex) {
+function addAnswer(answerText, qIdx, idx) {
   let a = document.querySelector(".answerBox");
   let answer = document.createElement("button");
   answer.classList.add("answerList");
@@ -24,8 +68,10 @@ function addAnswer(answerText, qIdex) {
   answer.classList.add("py-3");
   answer.classList.add("mx-auto");
   answer.classList.add("fadeIn");
+
   a.appendChild(answer);
   answer.innerHTML = answerText;
+
   answer.addEventListener(
     "click",
     function () {
@@ -36,27 +82,35 @@ function addAnswer(answerText, qIdex) {
         children[i].style.animation = "fadeOut 0.5s";
       }
       setTimeout(() => {
+        select[qIex] = idx;
+        let target = qnaList[qIdx].a[idx].type;
+        for (let i = 0; i < target.length; i++) {
+          select[target[i]] += 1;
+        }
+
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = "none";
         }
-        goNext(++qIdex);
+        goNext(++qIdx);
       }, 450);
     },
     false
   );
 }
 
-function goNext(qIex) {
-  if (++qIdx === endPoint) {
+function goNext(qIdx) {
+  if (qIdx + 1 === endPoint) {
     goResult();
+    return;
   }
+
   let q = document.querySelector(".qBox");
-  q.innerHTML = qnaList[qIex].q;
-  for (let i in qnaList[qIex].a) {
-    addAnswer(qnaList[qIex].a[i].answer, qIex);
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
-  let status = document.querySelector(".statusBar");
-  status.style.width = (100 / endPoint) * (qIex + 1) + "%";
+  var status = document.querySelector(".statusBar");
+  status.style.width = (100 / endPoint) * (qIdx + 1) + "%";
 }
 
 function begin() {
@@ -64,12 +118,12 @@ function begin() {
   main.style.animation = "fadeOut 1s";
   setTimeout(() => {
     qna.style.WebkitAnimation = "fadeIn 1s";
-    qna.style.animation = "fadeIut 1s";
+    qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
       main.style.display = "none";
       qna.style.display = "block";
     }, 450);
-    let qIdex = 0;
-    goNext(qIdex);
+    let qIdx = 0;
+    goNext(qIdx);
   }, 450);
 }
